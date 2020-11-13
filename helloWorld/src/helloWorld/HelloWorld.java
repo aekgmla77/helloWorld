8e5673cf15c55aa954f6fa98007c749882d6b52d
admin <admin@YD03-03>
2020-11-13 12:41:19 +0900

    첫번째

---
+++ helloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,19 @@
+public class Exercise04 {
+	public class OperatorExample {
+		public static void main(String[] args) {
+			
+		}
+		int pencils =534;
+		int students =30;
+		
+		
+		//학생 한 명이 가지는 연필 수 
+		int pencilesPerStudent = (pencils/students);
+		System.out.println(pencilesPerStudent);
+		
+		//남은 연필 수 
+		int pencilsLeft = (); 
+	    System.out.println(pencilsLeft);
+		
+	}
+}
